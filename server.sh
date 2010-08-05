22b7cf1ea984411feeb405363adc6fb058357119
German Ilyin <germanilyin@gmail.com>
2010-08-05 23:27:49 +0600

    stopping server properly

--- server.sh
+++ server.sh
@@ -2,13 +2,15 @@
 
 . ./deploy/$1/dirs.cfg
 
+uwsgi_cmd="$uwsgi_bin/uwsgi -s $socket -p 4 -M -t 20 -r -C -L -d ../wsgi_watchme.log wsgi"
+
 case $2 in
 "start")
-$uwsgi_bin/uwsgi -s $socket -p 4 -M -t 20 -r -C -L -d ../wsgi_watchme.log wsgi
+$uwsgi_cmd
 sudo $nginx_bin/nginx -c $nginx_conf
 ;;
 "stop")
-killall -9 uwsgi
+ps aux | grep '$uwsgi_cmd' | grep -v grep | awk '{system("kill -9 " $2)}'
 sudo $nginx_bin/nginx -s stop -c $nginx_conf
 ;;
 "restart")
