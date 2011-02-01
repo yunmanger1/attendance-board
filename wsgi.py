4cd4f7dd831ee755afad725e3dc53be4b103c7d6
George Anderson <yun_man_ger_docs@mail.ru>
2011-02-01 12:32:29 +0600

    downgrade

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,6 @@
 import sys,os
 
-sys.path.append('/home/german/distr/django-trunk')
-sys.path.append('/home/german/work')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'watchme.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'atboard.settings'
   
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
